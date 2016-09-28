10f4741d70165e4b989c8f819ae2ddc38822b12f
sawsan <sawsan.i.as@gmail.com>
2016-09-28 12:28:25 +0200

    adding the route handlers and components

---
+++ src/routes/Detail.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export default class Detail extends React.Component {
+  render() {
+    return <div> This is the detailed result of the item xxxxx</div>;
+
+  }
+}
---
+++ src/routes/Placeholder.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Placeholder extends React.Component {
+  render() {
+    return <div> Start searching by typing in the textbox in the navbar above </div>;
+  }
+}
