36704ec18158696cf10d259d408fdaef41b52cdd
sawsan <sawsan.i.as@gmail.com>
2016-09-28 12:26:48 +0200

    Define the main routes of the application

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Route } from 'react-router';
-import App from './AppRoute';
-
-class Useless extends React.Component {
-  render() {
-    return <div>I'm useless</div>;
-  }
-}
+import { Route, DefaultRoute } from 'react-router';
+import Home from './Home';
+import Placeholder from './Placeholder';
+import Search from './Search';
+import Detail from './Detail';
 
 export default (
-  <Route name='main' path='/'>
-    <Route name='counter' handler={App} path='counter' />
-    <Route name='useless' handler={Useless} path='useless' />
+  <Route name='main' handler={Home} path='/'>
+    <DefaultRoute name='placeholder' handler={Placeholder}/>
+    <Route name='search' handler={Search} path='search'>
+      <Route name='detail' handler={Detail} path=':id/detail'/>
+    </Route>
   </Route>
 );
