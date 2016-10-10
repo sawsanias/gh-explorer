b05a77753858a8dbdb8117ddb993273de7267707
sawsan <sawsan.i.as@gmail.com>
2016-10-10 17:38:18 +0200

    using the Scroll component instead of scrollview

--- src/components/Search/SearchResultPanel.js
+++ src/components/Search/SearchResultPanel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Panel from '../Panel';
-import ScrollView from '../ScrollView';
+import ScrollView from '../Scroll';
 import FlexView from 'react-flexview';
 
 const Card = ({ title, author, date, children }) => (
@@ -20,6 +20,28 @@ const Card = ({ title, author, date, children }) => (
 
 export default class SearchResultPanel extends React.Component {
 
+  state = { showLowerButton: false };
+
+  showButtons = (event) => {
+    this.setState({ showLowerButton: event.nativeEvent.target.scrollTop > 0 });
+  }
+
+  scrollToTop = () => {
+    this.scrollTo(0, 0, 500);
+  }
+
+  getContent = () => {
+    return (
+      <div>
+        {this.props.items.map((r, i) => {
+          return <Card key={i} title={r.title} author={r.author} date={r.date}>{r.description}</Card>;
+        })}
+        {this.state.showLowerButton && <button style={{ position: 'absolute', bottom: 0, right: 0 }} onClick={this.scrollToTop}>
+          Go to top
+        </button>}
+      </div>
+    );
+  }
   render() {
     const headerContent = (
       <div> Repositorises </div>
@@ -31,11 +53,21 @@ export default class SearchResultPanel extends React.Component {
 
     return (
       <Panel type='floating' header={panelHeader}>
-        <ScrollView>
-          {this.props.items.map((r, i) => {
-            return <Card key={i} title={r.title} author={r.author} date={r.date}>{r.description}</Card>;
-          })}
+        <ScrollView
+          easing='easeInOutQuad'
+          scrollX={false}
+          scrollPropagation={false}
+          onScroll={this.showButtons}
+        >
+          {(scrollTo) => {
+            this.scrollTo = scrollTo;
+            return this.getContent();
+          }}
         </ScrollView>
+
+
+
+
       </Panel>
     );
   }
