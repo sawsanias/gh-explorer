ec5b7be0cff54aa686e2cc1a7b7762bd60ee08b0
sawsan <sawsan.i.as@gmail.com>
2016-10-10 15:05:14 +0200

    using conditional rendering of the components

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import { RouteHandler } from 'react-router';
+import FlexView from 'react-flexview';
+import SearchResultPanel from 'components/SearchResultPanel';
+import EmptyResult from 'components/EmptyResult';
+import UndefinedResult from 'components/UndefinedResult';
+
+//TODO: To gete the json from the service
+const repositories = [
+  { name: 'Rep1', author: 'Sawsan', date: '12/01/2017', description: 'this is a test repository' },
+  { name: 'Rep2', author: 'Sawsan', date: '12/01/2017', description: 'this is a test repository' },
+  { name: 'Rep3', author: 'Sawsan', date: '12/01/2017', description: 'this is a test repository' },
+  { name: 'Rep4', author: 'Sawsan', date: '12/01/2017', description: 'this is a test repository' },
+  { name: 'Rep5', author: 'Sawsan', date: '12/01/2017', description: 'this is a test repository' },
+  { name: 'Rep6', author: 'Sawsan', date: '12/01/2017', description: 'this is a test repository' },
+  { name: 'Rep7', author: 'Sawsan', date: '12/01/2017', description: 'this is a test repository' },
+  { name: 'Rep8', author: 'Sawsan', date: '12/01/2017', description: 'this is a test repository' }
+];
 
 
 export default class Search extends React.Component {
+
   render() {
+    const undefinedResult = (typeof repositories === 'undefined' || repositories === null);
+    const emptyResult = !undefinedResult && repositories.length === 0;
+    const foundResult = !undefinedResult && repositories.length > 0; //simply !undefinedResult && !emptyResult
+
     return (
-      <div>
-        <div> my search results </div>
+      <FlexView hAlignContent='center' column>
+        {undefinedResult && <UndefinedResult />}
+        {emptyResult && <EmptyResult />}
+        {foundResult && <SearchResultPanel items={repositories} />}
         <RouteHandler />
-      </div>
+      </FlexView>
     );
   }
 }
