39ccd9bf844c3edda6edffab9e43f10e44602769
Giovanni Gonzaga <giovanni@buildo.io>
2016-10-10 11:35:28 +0200

    add components changes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,21 +1,38 @@
 import React from 'react';
-import NavBar from 'components/NavBar';
 import { RouteHandler } from 'react-router';
 import FlexView from 'react-flexview';
+import { NavBar, Button } from 'buildo-react-components';
 
 
 
 export default class Home extends React.Component {
+
+  onClick = () => alert('Search now!');
+  left = () => <span> GH Explorer </span>;
+  right = () => <Button primary icon='search' label='Search' onClick={this.onClick} />;
+  center = () =>   <input value={this.props.query} type='text' placeholder='Search repository' className='input-text' />
+
   render() {
+    const navBarProps = {
+      content: {
+        left: this.left(),
+        right: this.right(),
+        center: this.center()
+      },
+      background: 'red',
+      height: 100
+    };
+
     return (
       <FlexView column height='100%' width='100%'>
-        <NavBar />
+        <NavBar {...navBarProps }/>
         <FlexView hAlignContent='center' style={{ backgroundColor: 'grey' }} grow column>
           <FlexView grow style={{ backgroundColor: 'blue', maxWidth: 1000 }} width='100%'>
             <RouteHandler/>
           </FlexView>
         </FlexView>
       </FlexView>
+
   );
 
 
