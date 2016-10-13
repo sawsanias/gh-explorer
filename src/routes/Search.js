3e8784af0f896807e843d29fa4665aa794ee4cfc
sawsan <sawsan.i.as@gmail.com>
2016-10-13 16:56:30 +0200

    call github search API with hard-coded search string

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import { RouteHandler } from 'react-router';
+import axios from 'axios';
 import FlexView from 'react-flexview';
 import SearchResultPanel from 'components/Search/SearchResultPanel';
 import EmptyResult from 'components/Search/EmptyResult';
 import UndefinedResult from 'components/Search/UndefinedResult';
-import repositories from 'test/repositories.json';
 
 export default class Search extends React.Component {
 
+  state = { repositories: null };
+
+  assignRespositories = (response) => {
+    this.setState({ repositories: response.data.items });
+  }
+
+  componentDidMount() {
+    axios.get('http://api.github.com/search/repositories', { params: { q: 'test' } })
+      .then(this.assignRespositories)
+      .catch((error) => console.log(error)); // eslint-disable-line
+  }
+
   render() {
-    const undefinedResult = (typeof repositories === 'undefined' || repositories === null);
-    const emptyResult = !undefinedResult && repositories.length === 0;
-    const foundResult = !undefinedResult && repositories.length > 0; //simply !undefinedResult && !emptyResult
+    const undefinedResult = (typeof this.state.repositories === 'undefined' || this.state.repositories === null);
+    const emptyResult = !undefinedResult && this.state.repositories.length === 0;
+    const foundResult = !undefinedResult && this.state.repositories.length > 0; //simply !undefinedResult && !emptyResult
 
     return (
       <FlexView hAlignContent='center' column grow>
         {undefinedResult && <UndefinedResult />}
         {emptyResult && <EmptyResult />}
-        {foundResult && <SearchResultPanel items={repositories} />}
+        {foundResult && <SearchResultPanel items={this.state.repositories} />}
         <RouteHandler />
       </FlexView>
     );
