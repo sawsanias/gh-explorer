69a9505b40603daa6963211ade31451a03797100
sawsan <sawsan.i.as@gmail.com>
2016-10-14 15:02:15 +0200

    Handle the error response of the github rep. API

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -5,19 +5,24 @@ import FlexView from 'react-flexview';
 import SearchResultPanel from 'components/Search/SearchResultPanel';
 import EmptyResult from 'components/Search/EmptyResult';
 import UndefinedResult from 'components/Search/UndefinedResult';
+import ErrorResult from 'components/Search/ErrorResult';
 
 export default class Search extends React.Component {
 
-  state = { repositories: null };
+  state = { repositories: null, errorMsg: null };
 
   assignRespositories = (response) => {
-    this.setState({ repositories: response.data.items });
+    this.setState({ repositories: response.data.items, errorMsg: null });
+  }
+
+  errorHandler = () => {
+    this.setState({ repositories: null, errorMsg: 'Search failed!' });
   }
 
   fetchRepositories = (q) => {
     axios.get('http://api.github.com/search/repositories', { params: { q } })
     .then(this.assignRespositories)
-    .catch((error) => console.log(error)); // eslint-disable-line
+    .catch(this.errorHandler); // eslint-disable-line
   }
 
   componentDidMount() {
@@ -31,12 +36,14 @@ export default class Search extends React.Component {
   }
 
   render() {
-    const undefinedResult = (typeof this.state.repositories === 'undefined' || this.state.repositories === null);
-    const emptyResult = !undefinedResult && this.state.repositories.length === 0;
-    const foundResult = !undefinedResult && this.state.repositories.length > 0; //simply !undefinedResult && !emptyResult
+    const errorResult = this.state.errorMsg !== null;
+    const undefinedResult = !errorResult && (typeof this.state.repositories === 'undefined' || this.state.repositories === null);
+    const emptyResult = !errorResult && !undefinedResult && this.state.repositories.length === 0;
+    const foundResult = !errorResult && !undefinedResult && this.state.repositories.length > 0; //simply !undefinedResult && !emptyResult
 
     return (
       <FlexView hAlignContent='center' column grow>
+        {errorResult && <ErrorResult errorMsg={this.state.errorMsg} />}
         {undefinedResult && <UndefinedResult />}
         {emptyResult && <EmptyResult />}
         {foundResult && <SearchResultPanel items={this.state.repositories} />}
