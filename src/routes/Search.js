8df1cc12eda76af64ec27aa84146b05b110af415
sawsan <sawsan.i.as@gmail.com>
2016-10-17 11:41:56 +0200

    Adding the loading spinner to the search result panel

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { RouteHandler } from 'react-router';
 import axios from 'axios';
 import FlexView from 'react-flexview';
+import LoadingSpinner from '../components/LoadingSpinner';
 import SearchResultPanel from 'components/Search/SearchResultPanel';
 import EmptyResult from 'components/Search/EmptyResult';
 import UndefinedResult from 'components/Search/UndefinedResult';
@@ -9,18 +10,20 @@ import ErrorResult from 'components/Search/ErrorResult';
 
 export default class Search extends React.Component {
 
-  state = { repositories: null, errorMsg: null };
+  state = { repositories: null, errorMsg: null, isFetching: false };
 
   assignRespositories = (response) => {
-    this.setState({ repositories: response.data.items, errorMsg: null });
+    this.setState({ repositories: response.data.items, errorMsg: null, isFetching: false });
   }
 
   errorHandler = () => {
-    this.setState({ repositories: null, errorMsg: 'Search failed!' });
+    this.setState({ repositories: null, errorMsg: 'Search failed!', isFetching: false });
   }
 
   fetchRepositories = (q) => {
     if (typeof q !== 'undefined' && q !== null && q.trim() !== '') {
+      //reset the states
+      this.setState({ repositories: null, errorMsg: null, isFetching: true });
       axios.get('http://api.github.com/search/repositories', { params: { q } })
       .then(this.assignRespositories)
       .catch(this.errorHandler); // eslint-disable-line
@@ -42,11 +45,17 @@ export default class Search extends React.Component {
     const undefinedResult = !errorResult && (typeof this.state.repositories === 'undefined' || this.state.repositories === null);
     const emptyResult = !errorResult && !undefinedResult && this.state.repositories.length === 0;
     const foundResult = !errorResult && !undefinedResult && this.state.repositories.length > 0; //simply !undefinedResult && !emptyResult
-
     return (
       <FlexView hAlignContent='center' column grow>
+        {this.state.isFetching &&
+        (<div style={{ position: 'relative', height: 300 }}>
+          <LoadingSpinner
+            size={50}
+            message={{ content: 'Loading ...' }}
+          />
+        </div>)}
+        {undefinedResult && <UndefinedResult />} //should be shown when no search is done
         {errorResult && <ErrorResult errorMsg={this.state.errorMsg} />}
-        {undefinedResult && <UndefinedResult />}
         {emptyResult && <EmptyResult />}
         {foundResult && <SearchResultPanel items={this.state.repositories} />}
         <RouteHandler />
