c6be16d314e1d40a9daa92ce741ffe980dd7ca66
sawsan <sawsan.i.as@gmail.com>
2016-10-19 12:24:57 +0200

    after closing the modal, go back to previous url

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -6,15 +6,22 @@ import axios from 'axios';
 export default class Detail extends React.Component {
   state = { isOpen: false };
 
+  static contextTypes = {
+    router: React.PropTypes.func.isRequired
+  }
   open = () => {
     this.setState({ repository: null, isOpen: true });
   }
-  close = () => this.setState({ isOpen: false })
+  close = () => {
+    this.setState({ isOpen: false });
+    this.context.router.goBack();
+  }
   getModal = () => (
     <RepositoryDetailModal repository={this.state.repository} closeModal={this.close.bind(this)} />
   )
-
   fetchDetailedRepository = (owner, repos) => {
+    //show the modal only after the response
+    //TODO: in future open modal with spinner
     this.setState({ isOpen: false });
     if (owner && owner.trim() !== '' && repos && repos.trim() !== '') {
       axios.get(`https://api.github.com/repos/${owner}/${repos}`)
@@ -25,7 +32,8 @@ export default class Detail extends React.Component {
   componentDidMount() {
     this.fetchDetailedRepository(this.props.params.owner, this.props.params.repos);
   }
-
+  componentDidUpdate = () => {
+  }
   componentWillReceiveProps(nextProps) {
     if ( (nextProps.params.owner !== this.props.params.owner) ||  (nextProps.params.repos !== this.props.params.repos) ) {
       this.fetchDetailedRepository(nextProps.params.owner, nextProps.params.repos);
